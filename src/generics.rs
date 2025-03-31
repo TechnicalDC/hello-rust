b3bffb14c0472c1fc3be550a6e7bec138c9b50cd
Dilip Chauhan <chauhandilip305@gmail.com>
2025-03-31 20:24:13 +0530

    Updated

--- src/generics.rs
+++ src/generics.rs
@@ -1,2 +1,11 @@
 
-
+//pub fn testFunction<T>(list: &[T]) {
+//    let mut largest = &list[0];
+//
+//    for item in list {
+//        if item > largest {
+//            largest = item;
+//        }
+//    }
+//}
+//
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
+use serde::Serialize;
+use std::io::stdin;
+//use serde_json;
+
 pub mod modules;
 pub mod extras;
 pub mod generics;
 
+#[derive(Serialize)]
+struct Employee {
+    name: String,
+    //age: Option<i32>,
+    age: i32,
+    location: String,
+    department: String
+}
+
 fn main() {
-    extras::display_user();
+    //extras::display_user();
+    let mut emp: Employee;
+    let listen = stdin();
+    let mut json_str: String = String::new();
+    //let _ = listen.read_line(&mut json_str);
+
+    emp = Employee {
+        name: "dilip".to_string(),
+        //age: None,
+        //age: Some(28),
+        age: 28,
+        location: "mumbai".to_string(),
+        department: "qad".to_string()
+    };
+
+    println!("{:?}", emp.age);
+
+    //let json_string = serde_json::to_string(&emp);
+
+    //println!("{}", json_string);
 }
