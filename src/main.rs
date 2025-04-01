1dc9c810746d05f7b766b15e99f1cab25046ded5
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-01 11:02:28 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use serde::Serialize;
+use serde::{ Serialize, Deserialize, Serializer, Deserializer };
 use std::io::stdin;
-//use serde_json;
+use serde_json::Result;
 
 pub mod modules;
 pub mod extras;
 pub mod generics;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+#[derive(Serialize, Debug)]
 struct Employee {
     name: String,
-    //age: Option<i32>,
     age: i32,
     location: String,
     department: String
@@ -20,20 +26,43 @@ fn main() {
     let mut emp: Employee;
     let listen = stdin();
     let mut json_str: String = String::new();
-    //let _ = listen.read_line(&mut json_str);
+    let _ = listen.read_line(&mut json_str);
 
     emp = Employee {
         name: "dilip".to_string(),
-        //age: None,
-        //age: Some(28),
         age: 28,
         location: "mumbai".to_string(),
         department: "qad".to_string()
     };
 
+    //emp = serde_json::from_str(&json_str);
     println!("{:?}", emp.age);
 
-    //let json_string = serde_json::to_string(&emp);
+    //let outout = serde_json::to_string(&emp);
+    //println!("{:?}", outout);
 
-    //println!("{}", json_string);
+    let _ = typed_example();
+}
+
+fn typed_example() -> Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into a Person object. This is exactly the
+    // same function as the one that produced serde_json::Value above, but
+    // now we are asking it for a Person as output.
+    let p: Person = serde_json::from_str(data)?;
+
+    // Do things just like with any other Rust data structure.
+    println!("Please call {} at the number {}", p.name, p.phones[0]);
+
+    Ok(())
 }
