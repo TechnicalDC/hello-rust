f94ce962bac143761b2af5e6027848fb32528ec5
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-10 10:03:37 +0530

    updated

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -2,8 +2,8 @@ pub fn handle_error() {
     panic!("Errrroorrr....");
 }
 
-//pub fn handle_error() -> Result<T, E> {
+//pub fn handle_error() -> Result<()> {
 //    println!("handle_error");
 //
-//    Ok(())
+//    Ok(());
 //}
